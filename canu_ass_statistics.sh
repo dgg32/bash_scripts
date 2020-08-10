65b407ec9158fb90e8c67412ada701935572061c
Sixing Huang <dgg32@users.noreply.github.com>
2020-08-10 09:55:40 +0200

    init with canu stats

---
+++ canu_ass_statistics.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ass_file=$1
+
+printf "Assemble file size: $(du -h $ass_file |cut -f 1)\n\n"
+
+printf "Total contigs: $(grep -c '>' $ass_file)\n\n"
+
+printf "Total bps: $(grep '>' $ass_file | sed -E 's/.+len=([0-9]*).+/\1/' | paste -s -d+ - | bc )\n\n"
+
+printf "Total assembled reads: $(grep '>' $ass_file | sed -E 's/.+reads=([0-9]*).+/\1/' | paste -s -d+ - | bc )\n\n"
+
+printf "Lengths of top 10 longest contig:\n"
+printf "$(grep '>' $ass_file | sed -E 's/.+len=([0-9]*).+/\1/' | sort -g | tail -n 10)"
+printf "\n\n"
+
+printf "Reads of top 10 most covered contigs:\n"
+printf "$(grep '>' $ass_file | sed -E 's/.+reads=([0-9]*).+/\1/' | sort -g | tail -n 10)"
+printf "\n\n"
+
+
+printf "Lengths of top 10 circuluar longest contig:\n"
+printf "$(grep '>' $ass_file | grep 'suggestCircular=yes' | sed -E 's/.+len=([0-9]*).+/\1/' | sort -g | tail -n 10)"
+printf "\n\n"
